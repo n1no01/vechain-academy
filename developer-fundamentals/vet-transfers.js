46f48ce97513d9c474ec9ff5a961813f81832923
alexlofe <alejandro.lofe@gmail.com>
2025-07-30 16:16:29 +0100

    update

--- developer-fundamentals/vet-transfers.js
+++ developer-fundamentals/vet-transfers.js
@@ -2,16 +2,7 @@ import { ThorClient } from '@vechain/sdk-network';
 const thor = ThorClient.at('https://mainnet.vechain.org');
 
 const logs = await thor.logs.filterTransferLogs({
-  options: {
-    offset: 0,
-    limit: 3,
-  },
-  range: {
-    unit: 'block',
-    from: 0,
-    to: 20000000,
-  },
-  criteriaSet: [
+    criteriaSet: [
     {
       recipient: '0x000000000000000000000000000000000000dead',
 
@@ -20,6 +11,15 @@ const logs = await thor.logs.filterTransferLogs({
       sender: '0x19135a7c5c51950b3aa4b8de5076dd7e5fb630d4',
     },
   ],
+    range: {
+    unit: 'block',
+    from: 0,
+    to: 20000000,
+  },
+  options: {
+    offset: 0,
+    limit: 1,
+  },
   order: 'asc',
 });
 
